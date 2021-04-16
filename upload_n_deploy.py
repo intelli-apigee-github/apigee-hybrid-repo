4b5807d3a2739b5b56681b3e1f29f1a870fa6a7a
intelli-apigee-github <82087672+intelli-apigee-github@users.noreply.github.com>
2021-04-16 11:07:02 +0530

    Update upload_n_deploy.py
    updated latest changes

--- upload_n_deploy.py
+++ upload_n_deploy.py
@@ -9,15 +9,28 @@ class Upload:
 		super(Upload, self).__init__()
 		
 	
-	def upload_to_hybrid(path,filename,token,resource_type,resourceId):
+	def upload_to_hybrid(path,filename,token,org,resource_type,resourceId):
 		print("Starting to upload proxy in hybrid")
 		#resourceId="apiProxyId"
-		url = "https://apigee.googleapis.com/v1/organizations/apigee-hybrid-demo-305106/"+resource_type+"?action=import&name="+filename+"&"+resourceId+" ="+filename
+		url = "https://apigee.googleapis.com/v1/organizations/"+org+"/"+resource_type+"?action=import&name="+filename+"&"+resourceId+" ="+filename
 		payload={}
 		files=[('file',(filename+'_new.zip',open(path+"\\"+filename+"\\"+filename+"_new.zip",'rb'),'application/zip'))]
 		headers = {'Authorization': 'Bearer '+token}
 		response = requests.request("POST", url, headers=headers, data=payload, files=files)
-		print(response.text)
+		status_code = response.status_code
+		successful_upload = []
+		if status_code ==200:
+			successful_upload.append(filename)
+			print(successful_upload)
+			#print(response.text)
+		if status_code == 401:
+			print("ERROR : ------Please Check the Access Token ,you are Unauthorized -------")
+			print(response.text)
+			#exit()
+		if status_code == 400:
+			print("ERROR : ------The Zip Bundle Contains Error-------")
+			print(response.text)
+			#exit()
 
 
 class Deploy:
@@ -25,14 +38,24 @@ class Deploy:
 	def __init__(self):
 		super(Deploy, self).__init__()
 
-	def deploy_to_hybrid(evn,filename,token,resource_type):
+	def deploy_to_hybrid(evn,filename,token,org,resource_type):
 		print("Starting to Deploy proxies on hybrid ")
-		url = "https://apigee.googleapis.com/v1/organizations/apigee-hybrid-demo-305106/environments/"+evn+"/"+resource_type+"/"+filename+"/revisions/1/deployments"
+		url = "https://apigee.googleapis.com/v1/organizations/"+org+"/environments/"+evn+"/"+resource_type+"/"+filename+"/revisions/1/deployments"
 		payload={}
 		headers = {'Content-Type': 'application/json','Authorization': 'Bearer '+token}
 		response = requests.request("POST", url, headers=headers, data=payload)
-		print(response.text)
+		status_code = response.status_code
+		successful_deploy = []
+		if status_code ==200:
+			successful_deploy.append(filename)
+			print(successful_deploy)
+			#print(response.text)
+		if status_code == 401:
+			print("ERROR : ------Please Check the Access Token ,you are Unauthorized -------")
+			print(response.text)
+			#exit()
+		if status_code == 400:
+			print("ERROR : ------The Deployment has Errors-------")
+			print(response.text)
+			#exit()
 		
-
-
-
