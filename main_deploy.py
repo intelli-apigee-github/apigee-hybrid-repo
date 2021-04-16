8abcd0218bb547839150751e8e032027fdad8499
intelli-apigee-github <82087672+intelli-apigee-github@users.noreply.github.com>
2021-04-16 11:06:18 +0530

    Update main_deploy.py
    updated latest changes

--- main_deploy.py
+++ main_deploy.py
@@ -3,36 +3,60 @@ import os
 import shutil
 import zipfile as zp
 import re
+#from pyutil import filereplace
 import pyutil
 import json
 import requests
 import upload_n_deploy
 import zip_n_unzip
+import creating_kvm
 import edit_spike_arrest_policy
 import change_statistic_collector_policy
 
 
-filename="backend-credentials"
+filename=""
+current_path = ""
+bearer_token =""
 
-current_path = os.path.dirname(os.path.realpath(__file__))
-print(current_path)
-bearer_token ="ya29.a0AfH6SMAGILwb98O-Oy9NgXfMZtKosbN69quD3Y36ORlKHGAK8myoXnuQLCUBondCNHmBCrLPnW4xkgx-b3lStbmRNWyy-wSfyiUIz5DUsC44AMJxjC3gr03UUU5vWI8KISDtP5VsMefeYwyPCJU6aecZeZGxNuolQkrKAvOI28oB5gXO6C7nlWgBJq54u3gXn6MnO1lc8KNLGaHKh8Yj1Qx1torG1opj6jI4WtckvfSzGwtI38COzMZkuU3JQjHl4AVeag"
-
+with open("config.json") as json_data_file:
+    data = json.load(json_data_file)
+current_path= data['path']
+bearer_token =data['token']
+org=data['org_name']
+evn=data['environment']
+count_of_resources=0
 
 ############################################ Upload and deploy Multiple Shared Flows #####################################
-#shareflows=["SF-quota"]
-#resource_type="sharedflows"
-#resource_id = "sharedflowId"
-#for filename in shareflows:
-	#zip_n_unzip.Unzip.unzip_file(filename)
-	#zip_n_unzip.Zip.create_newzip_after_changes(filename,current_path)
-	#upload_zip=upload_n_deploy
-	#upload_n_deploy.Upload.upload_to_hybrid(current_path,filename,bearer_token,resource_type,resource_id)
-	#upload_n_deploy.Deploy.deploy_to_hybrid("test",filename,bearer_token,resource_type)
+shareflows_folder_path=current_path+"\\sharedflows"
+resource_type="sharedflows"
+resource_id = "sharedflowId"
+filenames = os.listdir(shareflows_folder_path)
+for filename in filenames:
+	filename =filename.strip()
+	filename =re.search(r'(.*?).zip',filename)
+	if filename:
+		filename = str(filename.group(1))
+		print(filename)
+		count_of_resources=count_of_resources +1
+		zip_n_unzip.Unzip.unzip_file(filename,shareflows_folder_path+"\\"+filename)
+		zip_n_unzip.Zip.create_newzip_after_changes(filename,shareflows_folder_path)
+		upload_zip=upload_n_deploy
+		upload_n_deploy.Upload.upload_to_hybrid(shareflows_folder_path,filename,bearer_token,org,resource_type,resource_id)
+		upload_n_deploy.Deploy.deploy_to_hybrid(evn,filename,bearer_token,org,resource_type)
 
-
+print("==================================================")
+print("| Completed uploading and Deploying Shared Flows |")
+print("|                                                |")
+print("| Starting to Create KVM                         |")
+print("==================================================")
 ################################################## Creating KVM 's ########################################################
-
+createkvm=creating_kvm
+kvm_folder_path="kvm"
+createkvm.Createkvm.createkvm(current_path,kvm_folder_path,bearer_token,org)
+print("=================================================")
+print("| Completed Creating KVM                         |")
+print("|                                                |")
+print("==================================================")
 
 ########################################## Editing spike arest policy ######################################################
 #filenames =['demo_spike_arrest_8','demo_spike_arrest_4','demo_spike_arrest_5','demo_spike_arrest_6','demo_spike_arrest_7']
@@ -51,32 +75,25 @@ bearer_token ="ya29.a0AfH6SMAGILwb98O-Oy9NgXfMZtKosbN69quD3Y36ORlKHGAK8myoXnuQLC
 	#statistic_collector.Main_static_collector.call_static_collector(current_path,filename,bearer_token,resource_type,resource_id)
 
 ################################################ Deploy multiple proxies at once ##############################################
-#list = [
-		#"access_control_demo","accu-my-account","accuweather","AM-Add-Set","AM-copy-demo","AM-demo1",
-		#"AM-new_req_obj","AM-prac_apikey","AM-remove-demo","BA-auth-demo","cache-demo","client_crentials_demo",
-		#"condition-demo","cors-demo","cors-demo2","cors-demo3","cors-demo44","cors-enabled",
-		#"CP-concurrent-demo","demo_populate_cache","DF-demo","edgemicro_hellojson","eight_demo_apigee","EV-demo1",
-		#"EV-MV-demo","EV-uri","final_flow","first_demo_apigee","flows_demo","fourth_demo_apigee",
-		#"FR_Execution_order","FR-custom","generate_oauth_userapi","getstarted","hello-hosted-targets","HT-demo",
-		#"JSON-TP-demo","JS-raise_error","JS-sum_demo","JS-url-concat","JWT-demo","Jwt-demo1",
-		#"JWT-verify","KVM-cache-demo","KVM-encrypted-demo","KVM-get-demo","KVM-update-insert","lab2a-v1",
-		#"lab3a-v1","lab5a-v1","lab6a-v1","lab8a-v1","LB-Demo","Login-API",
-		#"login-ro-api-prac","mock","mock2","mul_PE_Ep","mul_pe_te","multi_proxy_enpoints"
+proxies_folder_path=current_path+"\\proxies"
+try:
+	filenames = os.listdir(proxies_folder_path)
+	resource_type="apis"
+	resource_id = "apiProxyId"
+	for filename in filenames:
+		filename =filename.strip()
+		filename =re.search(r'(.*?).zip',filename)
+		if filename:
+			count_of_resources=count_of_resources +1
+			filename = str(filename.group(1))
+			if count_of_resources <=50:
+				print(filename)
+				print("Total Resources Count "+str(count_of_resources))
+				zip_n_unzip.Unzip.unzip_file(filename,proxies_folder_path+"\\"+filename)
+				zip_n_unzip.Zip.create_newzip_after_changes(filename,proxies_folder_path)
+				upload_zip=upload_n_deploy
+				upload_zip.Upload.upload_to_hybrid(proxies_folder_path,filename,bearer_token,org,resource_type,resource_id)
+				upload_zip.Deploy.deploy_to_hybrid(evn,filename,bearer_token,org,resource_type)
 
-#]
-
-#list = ["fourth_demo_apigee"]
-resource_type="apis"
-resource_id = "apiProxyId"
-list =["fourth_demo_apigee"]
-for filename in list:
-	zip_n_unzip.Unzip.unzip_file(filename)
-	zip_n_unzip.Zip.create_newzip_after_changes(filename,current_path)
-	upload_zip=upload_n_deploy
-	upload_zip.Upload.upload_to_hybrid(current_path,filename,bearer_token,resource_type,resource_id)
-	upload_zip.Deploy.deploy_to_hybrid("test1",filename,bearer_token,resource_type)
-
-
-
-
-
+except IOError:
+    print('File not found at the specified location in config.json file')
