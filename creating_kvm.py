2bd4bd31151244d6c49a42251f5ab1ba418abf6e
intelli-apigee-github <82087672+intelli-apigee-github@users.noreply.github.com>
2021-04-23 18:49:20 +0530

    Update creating_kvm.py

--- creating_kvm.py
+++ creating_kvm.py
@@ -8,14 +8,7 @@ import json
 import requests
 import upload_n_deploy
 import zip_n_unzip
-import edit_spike_arrest_policy
-import change_statistic_collector_policy
 
-
-#filename="kvm"
-#env=""
-#bearer_token="ya29.a0AfH6SMDjJfitSOYK_LXvyr-ctPr6g6cTIy_Gjvx7nuJSnPArog0sb7ESd4MdANuzN81_u_iK5iajYGliJLaHG0jqB43Mqor8Tpne3LDc3AVolP4aIrkMh_Bj8luEKRZUwCIDfyaSaWkJfGMaXZl8EhWg1dNxC0Z2RN6zogZBtooLhD776MoYSbtOhhXTfPlrS-tp4PKYN_2N8Vw20701kAvov9Inqknc_ZHv4Nm1Dju_AniHH_qwTYv16mGRZvodfwKBiGI"
-#current_path = os.path.dirname(os.path.realpath(__file__))
 
 class Createkvm:
 	"""docstring for Createkvm"""
@@ -32,7 +25,6 @@ class Createkvm:
 					match_evn=re.search(r'env\\(.*?)\\',cur_path)
 					if match_evn:
 						env=match_evn.group(1)
-						print("KVM  " +file+" created in " +env+ " environment")
 						url = "https://apigee.googleapis.com/v1/organizations/"+org+"/environments/"+env+"/keyvaluemaps"
 						payload = "{\r\n  \"encrypted\": true,\r\n  \"name\": \""+file+"\"\r\n}"
 						headers = {'Authorization': 'Bearer '+token,'Content-Type': 'text/plain'}
@@ -40,11 +32,18 @@ class Createkvm:
 						#print(response.text)
 						status_code = response.status_code
 						if(status_code !=201):
-							print("Error while Creating KVM")
+							print("-------------------------------------------------------------------------------------------")
+							print("|                         Error while creating "+file+" KVM                                ")
+							print("-------------------------------------------------------------------------------------------")
+
 							print(response.text)
-							exit()
+							
 						else:
-							print(response.text)	
+							#print(response.text)
+							print("-------------------------------------------------------------------------------------------")
+							print("|   KVM  " +file+" created in " +env+ " environment")
+							print("-------------------------------------------------------------------------------------------")
+	
 
 
 
